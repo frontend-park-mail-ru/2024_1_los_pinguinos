490b0b860317f121682f71d5c201b54bb7ca62c1
Fedor Dolmatov <l1sZ@ya.ru>
2024-03-11 16:16:09 +0300

    header login/logout button now works

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,11 +1,21 @@
 import apiHandler from '../../api/apiHandler';
-
+/**
+ * Router class
+ * @class
+ */
 export class Router {
+  /**
+   * Creates instance of class Router.
+   * @param {Route[]} routes - list of specified routes
+   */
   constructor(routes) {
     this.routes = routes;
     this.init();
   }
-
+  /**
+   * Initializes DOM event listeners to capture page rerouting.
+   * @function
+   */
   init() {
     window.addEventListener('load', () => this.loadRoute());
     window.addEventListener('popstate', () => this.loadRoute());
@@ -16,12 +26,19 @@ export class Router {
       }
     });
   }
-
+  /**
+   * Redirect to specified page without reload
+   * @function
+   * @param {string} url - navigation url
+   */
   navigateTo(url) {
     history.pushState(null, null, url);
     this.loadRoute();
   }
-
+  /**
+   * Loads route components and adds them into root inner html, redirects to specified route depending on auth status
+   * @function
+   */
   async loadRoute() {
     const route =
       this.routes.find((r) => r.path === location.pathname) ||
@@ -32,10 +49,16 @@ export class Router {
     if (route.component.controller) {
       await route.component.controller();
     }
-    const logoutButton = document.getElementById('header__button');
+    const logoutButton = document.getElementById('header__button--logout');
     if (logoutButton) {
       logoutButton.addEventListener('click', () => {
         apiHandler.Logout();
+      });
+    }
+    const loginButton = document.getElementById('header__button');
+    if (loginButton) {
+      loginButton.addEventListener('click', () => {
+        this.navigateTo('/login');
       });
     }
 
@@ -52,8 +75,18 @@ export class Router {
     }
   }
 }
-
+/**
+ * Route class
+ * @class
+ */
 export class Route {
+  /**
+   * Creates instance of class Route.
+   * @param {string} path - request url
+   * @param {Object} component - page component
+   * @param {boolean} protect - wheter the route is protected
+   * @param {string} redirectURL - redirection url
+   */
   constructor(path, component, protect = false, redirectURL = null) {
     this.path = path;
     this.component = component;
