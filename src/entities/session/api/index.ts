58c60fe9663a224038e7b66b04ffd18deefe2a01
DeDeimos <rodion.tolkachev.03@inbox.ru>
2024-04-27 00:21:45 +0300

    new reg logic

--- src/entities/session/api/index.ts
+++ src/entities/session/api/index.ts
@@ -5,6 +5,10 @@ export const login = async (email: string, password: string): Promise<SessionRes
     return sendRequest('/login', 'POST', { email, password });
 }
 
+export const register = async (email: string, password: string, name: string, age: number, interests: string[]): Promise<SessionResponse> => {
+    return sendRequest('/register', 'POST', { email, name, age, interests, password });
+}
+
 export const logout = async () => {
     return sendRequest('/logout', 'POST');
 };
--- src/pages/register/registr.jsx
+++ src/pages/register/registr.jsx
@@ -1,19 +1,72 @@
 import Header from '../../widgets/header/index';
 import { useState } from '../../reactor/index';
 import InterestsInput from '../../widgets/interestsInput';
+import { register } from '../../entities/session/api';
 
 export const Registeration = () => {
-    const [form1, setForm1] = useState(false);
+    const [form1, setForm1] = useState(true);
     const [form2, setForm2] = useState(false);
-    const [form3, setForm3] = useState(true);
+    const [form3, setForm3] = useState(false);
     const [form4, setForm4] = useState(false);
+    const [error, setError] = useState(false);
 
-    const interests = [
-        'Игры',
-        'Музыка',
-        'Кино',
-        'Технологии',
-    ];
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [sex, setSex] = useState('');
+    const [password, setPassword] = useState('');
+
+    const from1to2 = () => {
+        console.log('from1to2');
+        setForm1(false);
+        setForm2(true);
+    };
+
+    const from2to3 = () => {
+        console.log('from2to3');
+        setForm2(false);
+        setForm3(true);
+    };
+
+    const from3to4 = () => {
+        console.log('from3to4');
+        setForm3(false);
+        setForm4(true);
+    };
+
+    const from4to3 = () => {
+        console.log('from4to3');
+        setForm4(false);
+        setForm3(true);
+    };
+
+    const from3to2 = () => {
+        console.log('from3to2');
+        setForm3(false);
+        setForm2(true);
+    };
+
+    const from2to1 = () => {
+        console.log('from2to1');
+        setForm2(false);
+        setForm1(true);
+    };
+
+    const registerUser = async () => {
+        try {
+            await register({
+                email: email,
+                name: name,
+                age: age,
+                interests: selectedInterests,
+                password: password,
+            });
+        } catch (e) {
+            setError(true);
+        }
+    };
+
+    const interests = ['Игры', 'Музыка', 'Кино', 'Технологии'];
 
     const [selectedInterests, setSelectedInterests] = useState(['Кино']);
 
@@ -25,63 +78,76 @@ export const Registeration = () => {
                     <div className="form">
                         <div className="form__field">
                             <label htmlFor="name">Почта</label>
-                            <input type="text" id="email" />
+                            <input
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="text"
+                                id="email"
+                            />
                         </div>
+                        <button onClick={from1to2}>Next</button>
                     </div>
                 )}
                 {form2 && (
                     <div className="form">
+                        <button onClick={() => console.log(234)}>Back</button>
                         <div className="form__field">
                             <label htmlFor="name">Имя</label>
-                            <input type="text" id="name" />
+                            <input
+                                onChange={(e) => setName(e.target.value)}
+                                type="text"
+                                id="name"
+                            />
                         </div>
                         <div className="form__field">
-                            <label htmlFor="surname">Возраст</label>
-                            <input type="date" id="age" />
+                            <label htmlFor="age">Возраст</label>
+                            <input
+                                onChange={(e) => setAge(e.target.value)}
+                                type="date"
+                                id="age"
+                            />
                         </div>
                         <div className="form__field">
-                            <label htmlFor="surname">Пол</label>
-                            <input type="text" id="sex" />
+                            <label htmlFor="sex">Пол</label>
+                            <input
+                                onChange={(e) => setSex(e.target.value)}
+                                type="text"
+                                id="sex"
+                            />
                         </div>
+
+                        <button onClick={from2to3}>Next</button>
                     </div>
                 )}
                 {form3 && (
-                    <div>
-                        <div className="form">
-                            <div className="form__field">
-                                <label htmlFor="name">Интересы</label>
-                                <InterestsInput
-                                    interests={interests}
-                                    selectedInterests={selectedInterests}
-                                    setSelectedInterests={setSelectedInterests}
-                                />
-                            </div>
+                    <div className="form">
+                        <button onClick={from3to2}>Back</button>
+                        <div className="form__field">
+                            <label htmlFor="name">Интересы</label>
+                            <InterestsInput
+                                interests={interests}
+                                selectedInterests={selectedInterests}
+                                setSelectedInterests={setSelectedInterests}
+                            />
                         </div>
-                        <button
-                            onClick={() => {
-                                setForm3(false);
-                                setForm1(true);
-                            }}
-                        >
-                            Next
-                        </button>
+                        <button onClick={from3to4}>Next</button>
                     </div>
                 )}
                 {form4 && (
-                    <div>
-                        <div className="form">
-                            <div className="form__field">
-                                <label htmlFor="name">Пароль</label>
-                                <input type="password" id="password" />
-                            </div>
+                    <div className="form">
+                        <button onClick={from4to3}>Back</button>
+                        <div className="form__field">
+                            <label htmlFor="name">Пароль</label>
+                            <input
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                id="password"
+                            />
                         </div>
-                        <button
-                            onClick={() => {
-                                setForm3(false);
-                                setForm1(true);
-                            }}
-                        >
-                            Next
+                        {error && (
+                            <div className="form__error">Произошла ошибка</div>
+                        )}
+                        <button onClick={registerUser}>
+                            Зарегестироваться
                         </button>
                     </div>
                 )}
@@ -89,4 +155,3 @@ export const Registeration = () => {
         </div>
     );
 };
-
