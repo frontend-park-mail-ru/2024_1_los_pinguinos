1414909e3adda2ade5fffc21493206c478efdc11
DeDeimos <rodion.tolkachev.03@inbox.ru>
2024-04-27 10:35:06 +0300

    new card api logic

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import { Router, Route } from './src/app/Router';
 import Login from './src/pages/login/login';
 import { Landing } from './src/pages/landing/landing';
 import { Register } from './src/pages/register/register';
+import Mainpage from './src/pages/main/main'
 
 // import error404 from './src/pages/404/404.js';
 // import Profile from './src/pages/profile/profile.js';
@@ -13,6 +14,7 @@ const routes = [
     new Route('/', Landing, false, '/main'),
     new Route('/login', Login, false, '/main'),
     new Route('/register', Register, false, '/main'),
+    new Route('/main', Mainpage, false, '/main'),
     // new Route('/register', new Register(), false, '/main'),
     // new Route('/main', new Home(), true),
     // new Route('/profile', new Profile(), true),
--- src/entities/person/api/index.ts
+++ src/entities/person/api/index.ts
@@ -4,16 +4,16 @@ import { Person } from "../model";
 import { normalizePerson } from "../lib/normalizePerson";
 
 export const getCards = async (): Promise<Person[]> => {
-    const cards = await sendRequest<PersonDTO[]>("GET", "/cards");
+    const cards = await sendRequest<PersonDTO[]>('/cards', 'GET');
     return cards.map(normalizePerson);
 };
 
 export const getMatches = async (): Promise<Person[]> => {
-    const matches = await sendRequest<PersonDTO[]>("GET", "/matches");
+    const matches = await sendRequest<PersonDTO[]>(`/matches`, 'GET');
     return matches.map(normalizePerson);
 }
 
 export const getProfile = async (): Promise<Person> => {
-    const profile = await sendRequest<PersonDTO>("GET", "/profile");
+    const profile = await sendRequest<PersonDTO>(`/profile`, 'GET');
     return normalizePerson(profile);
 }
